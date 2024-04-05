0aabda3d38bf821391339926b7129122f4b390cf
nammtph52069 <nammtph52069>
2024-04-06 04:34:39 +0700

    dark-mode light-mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,61 @@
-// import Button from '@mui/material/Button'
+import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography'
+import { useColorScheme } from '@mui/material/styles'
 
-function App() {
+import Box from '@mui/material/Box'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
+import LightModeIcon from '@mui/icons-material/LightMode'
+import NightsStayIcon from '@mui/icons-material/NightsStay'
+import ContrastIcon from '@mui/icons-material/Contrast'
+
+function ModeSelect() {
+  const { mode, setMode } = useColorScheme()
+
+  // Handle change dark-mode or light-mode of website
+  const handleChange = (event) => {
+    const selectedMode = event.target.value
+    setMode(selectedMode)
+  }
 
   return (
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="label-select-mode">Mode</InputLabel>
+      <Select
+        labelId="label-select-mode"
+        id="select-mode"
+        value={mode}
+        label="Mode"
+        onChange={handleChange}
+      >
+        <MenuItem value="light">
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+            <LightModeIcon fontSize='small' /> Light
+          </Box>
+        </MenuItem>
+        <MenuItem value="dark">
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+            <NightsStayIcon fontSize='small' /> Dark
+          </Box>
+        </MenuItem>
+        <MenuItem value="system">
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+            <ContrastIcon fontSize='small' /> System
+          </Box>
+        </MenuItem>
+      </Select>
+    </FormControl>
+  )
+}
+
+
+function App() {
+  return (
     <>
-      <Typography variant="body2" color="text.secondary">dvdvdv</Typography>
+      <ModeSelect />
+      <Typography variant="body2" color="text.secondary"></Typography>
 
     </>
   )
