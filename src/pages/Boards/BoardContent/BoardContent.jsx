d1ff586d29afd2801bc442ad3d7b4dcdf48d051d
nammtph52069 <nammtph52069>
2024-04-14 01:59:10 +0700

    create MockData and Drag-drop

--- src/pages/Boards/BoardContent/BoardContent.jsx
+++ src/pages/Boards/BoardContent/BoardContent.jsx
@@ -1,17 +1,64 @@
 import Box from '@mui/material/Box'
 import ListColumns from './ListColumns/ListColumns'
+import { mapOrder } from '~/utils/sorts'
+import {
+  DndContext,
+  // PointerSensor,
+  useSensor,
+  useSensors,
+  MouseSensor,
+  TouchSensor
+} from '@dnd-kit/core'
+import { useEffect, useState } from 'react'
+import { arrayMove } from '@dnd-kit/sortable'
 
-function BoardContent() {
+function BoardContent({ board }) {
+  const [orderedColumns, setOrderedColumns] = useState([])
+
+  // yêu cầu di chuyển 10px thì mới gọi event
+  // const pointerSensor = useSensor(PointerSensor, { activationConstraint: { distance: 10 } })
+  // const sensors = useSensors(pointerSensor)
+  // Nên sử dụng cách này để tránh bị bug trên mobile và laptop
+  const mouseSenser = useSensor(MouseSensor, { activationConstraint: { distance: 10 } })
+  const touchSensor = useSensor(TouchSensor, { activationConstraint: { delay: 250, tolerance: 5 } })
+  const sensors = useSensors(mouseSenser, touchSensor)
+
+  useEffect(() => {
+    setOrderedColumns(mapOrder(board?.columns, board?.columnOrderIds, '_id'))
+  }, [board])
+
+  const handleDregEnd = (event) => {
+    const { active, over } = event
+
+    // nếu over không tồn tại thì return tránh lỗi
+    if (!over) {return}
+
+    // Nếu vị trí sau khi kéo thả khác vị trí ban đầu
+    if (active.id !== over.id) {
+      const oldIndex = orderedColumns.findIndex(c => c._id === active.id) // lay vi tri cu tu active
+      const newIndex = orderedColumns.findIndex(c => c._id === over.id) // lay vi tri moi tu over
+
+      const dndOrderedColumns = arrayMove(orderedColumns, oldIndex, newIndex)
+      // const dndOrderedColumnsIds = dndOrderedColumns.map(c => c._id)
+      // console.log('dndOrderedColumns:', dndOrderedColumns);
+      // console.log('dndOrderedColumnsIds:', dndOrderedColumnsIds);
+
+      // Cập nhật lại state columns ban đầu sau khi kéo thả
+      setOrderedColumns(dndOrderedColumns)
+    }
+  }
 
   return (
-    <Box sx={{
-      bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#34495e' : '#1976d2'),
-      width: '100%',
-      height: (theme) => theme.trello.boardContentHeight,
-      p: '10px 0'
-    }}>
-      <ListColumns />
-    </Box>
+    <DndContext sensors={sensors} onDragEnd={handleDregEnd}>
+      <Box sx={{
+        bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#34495e' : '#1976d2'),
+        width: '100%',
+        height: (theme) => theme.trello.boardContentHeight,
+        p: '10px 0'
+      }}>
+        <ListColumns columns={orderedColumns} />
+      </Box>
+    </DndContext>
   )
 }
 
--- src/pages/Boards/BoardContent/ListColumns/Columns/ListCards/Card/Card.jsx
+++ src/pages/Boards/BoardContent/ListColumns/Columns/ListCards/Card/Card.jsx
@@ -1,4 +1,4 @@
-import {Card as MuiCard} from '@mui/material'
+import { Card as MuiCard } from '@mui/material'
 import CardActions from '@mui/material/CardActions'
 import CardContent from '@mui/material/CardContent'
 import CardMedia from '@mui/material/CardMedia'
@@ -8,19 +8,9 @@ import AttachmentIcon from '@mui/icons-material/Attachment'
 import Typography from '@mui/material/Typography'
 import Button from '@mui/material/Button'
 
-function Card({ temporaryHideMedia }) {
-  if (temporaryHideMedia) {
-    return (
-      <MuiCard sx={{
-        cursor: 'pointer',
-        boxShadow: '0 1px 1px rgba(0, 0, 0, 0.2)',
-        overflow: 'unset'
-      }}>
-        <CardContent sx={{ p: 1.5, '&:last-child': { p: 1.5 } }}>
-          <Typography> Card 01 </Typography>
-        </CardContent>
-      </MuiCard>
-    )
+function Card({ card }) {
+  const shouldShowCardAction = () => {
+    return !!card?.memberIds?.length || !!card?.comments?.length || !!card?.attachments?.length
   }
   return (
     <MuiCard sx={{
@@ -28,19 +18,17 @@ function Card({ temporaryHideMedia }) {
       boxShadow: '0 1px 1px rgba(0, 0, 0, 0.2)',
       overflow: 'unset'
     }}>
-      <CardMedia
-        sx={{ height: 140 }}
-        image="https://yt3.ggpht.com/ytc/AIdro_nLaihxDFVmnJ3BNrRAmxQrbA_mCKWJCT4LKngxCiB_0GgMeT12zvtvULsylDTrrmZaZg=s88-c-k-c0x00ffffff-no-rj"
-        title="green iguana"
-      />
+      {card?.cover && <CardMedia sx={{ height: 140 }} image={card?.cover} /> }
       <CardContent sx={{ p: 1.5, '&:last-child': { p: 1.5 } }}>
-        <Typography> The nam 2kx </Typography>
+        <Typography>{card?.title}</Typography>
       </CardContent>
-      <CardActions sx={{ p: '0 4px 8px 4px' }}>
-        <Button size="small" startIcon={ <GroupIcon /> }>20</Button>
-        <Button size="small" startIcon={ <CommentIcon /> }>10</Button>
-        <Button size="small" startIcon={ <AttachmentIcon /> }>20</Button>
-      </CardActions>
+      {shouldShowCardAction() &&
+        <CardActions sx={{ p: '0 4px 8px 4px' }}>
+          {!!card?.memberIds?.length && <Button size="small" startIcon={ <GroupIcon /> }>{card?.memberIds?.length}</Button> }
+          {!!card?.comments?.length && <Button size="small" startIcon={ <CommentIcon /> }>{card?.comments?.length}</Button> }
+          {!!card?.attachments?.length && <Button size="small" startIcon={ <AttachmentIcon /> }>{card?.attachments?.length}</Button> }
+        </CardActions>
+      }
     </MuiCard>
   )
 }
